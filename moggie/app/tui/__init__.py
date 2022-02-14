a1be73af9d47e8cc9e164a8afb8b8c74eea2b211
Bjarni R. Einarsson <bre@klaki.net>
2022-02-14 15:00:19 +0000

    TUI progress

--- moggie/app/tui/__init__.py
+++ moggie/app/tui/__init__.py
@@ -1,53 +1,22 @@
 import asyncio
+import datetime
+import json
 import random
 import time
 import urwid
+import traceback
 
 import websockets
 import websockets.exceptions
 
 from ...config import APPNAME, APPVER
 from ...email.metadata import Metadata
+from ...jmap.core import JMAPSessionResource
+from ...jmap.requests import *
 from ...util.rpc import AsyncRPCBridge
-from ...workers.app import AppWorker, test_contexts, test_emails
-
-
-ENVELOPES = ("""\
-     _______      x
-    |==   []|     x
-    |  ==== |____ x
-    '-------'  []|x
-         |   === |x
-         '-------'x
-  _______         x
- |==   []|        x
- |  ==== |        x
- '-------'        x
-""").replace('x', '')
-
-HELLO = ("""\
-  _                        x
-  \`*-.                    x
-   )  _`-.                 x
-  .  : `. .                x
-  : _   '  \               x
-  ; *` _.   `*-._          x
-  `-.-'          `-.       x
-    ;       `       `.     x
-    :.       .        \    x
-    . \  .   :   .-'   .   x
-    '  `+.;  ;  '      :   x
-    :  '  |    ;       ;-. x
-    ; '   : :`-:     _.`* ;x
-.*' /  .*' ; .*`- +'  `*'  x
- `*-*   `*-*  `*-*'        x
-
-%s v%s                     x
-""").replace('x', '') % (APPNAME, APPVER)
-
-HELLO_CREDITS = """
-   (cat by Blazej Kozlowski)
-"""
+from ...workers.app import AppWorker
+from ..core import test_contexts, test_emails
+from .decorations import palette, ENVELOPES, HELLO, HELLO_CREDITS
 
 
 def _w(w, attr={}, valign='top'):
@@ -73,25 +42,99 @@ class Selectable(urwid.WidgetWrap):
         return key
 
 
-class EmailList(urwid.ListBox):
-    def __init__(self, tui_frame, emails):
-        self.emails = emails
+class EmailDisplay(urwid.Filler):
+    def __init__(self, tui_frame, metadata, parsed):
+        self.metadata = Metadata(*metadata)
+        self.parsed = self.metadata.parsed()
+        self.email = parsed
+
+        urwid.Filler.__init__(self, urwid.Text(
+            [self.parsed.get('subject', 'ohai')], 'center'),
+            valign='middle')
+
+
+class EmailListWalker(urwid.ListWalker):
+    def __init__(self, tui_frame, mailbox, emails=None):
+        self.focus = 0
+        self.mailbox = mailbox
+        self.emails = emails or []
         self.tui_frame = tui_frame
+        self.app_bridge = tui_frame.app_bridge
+        self.loading = 0
+        self.want_more = len(self.emails) < 100
+        self._load_more()
 
-        def _sel_email(which):
-            return lambda x: self.tui_frame.show_email(which)
+    def __len__(self):
+        return len(self.emails)
 
-        widgets = []
-        for i, email in enumerate(emails):
+    def _load_more(self):
+        now = time.time()
+        if (self.loading > now - 5) or not self.want_more:
+            return
+        self.loading = time.time()
+        self.app_bridge.send_json(RequestMailbox(self.mailbox,
+            skip=len(self.emails),
+            limit=min(max(500, 2*len(self.emails)), 10000)))
+
+    def incoming_message(self, message):
+        if (message.get('prototype') != 'mailbox' or
+                message.get('mailbox') != self.mailbox):
+            return
+
+        try:
+            self.emails[message['skip']:] = message['emails']
+            self.emails.sort()
+            self.emails.reverse()
+
+            self.want_more = (message['limit'] == len(message['emails']))
+            self.loading = 0
+            self._load_more()
+
+            self._modified()
+        except:
+            traceback.print_exc()
+
+    def set_focus(self, focus):
+        self.focus = focus
+        if focus > len(self.emails) - 100:
+            self._load_more()
+
+    def next_position(self, pos):
+        if pos + 1 < len(self.emails):
+            return pos + 1
+        self._load_more()
+        raise IndexError
+
+    def prev_position(self, pos):
+        if pos > 0:
+            return pos - 1
+        raise IndexError
+
+    def positions(self, reverse=False):
+        if reverse:
+            return reversed(range(0, len(self.emails)))
+        return range(0, len(self.emails))
+
+    def __getitem__(self, pos):
+        try:
+            md = Metadata(*self.emails[pos]).parsed()
+            frm = md.get('from', {})
+            frm = frm.get('fn') or frm.get('address') or '(none)'
+            attrs = '(    )'
+            subj = md.get('subject', '(no subject)')
+            dt = datetime.datetime.fromtimestamp(md.get('ts', 0))
+            dt = dt.strftime('%Y-%m-%d')
             cols = urwid.Columns([
-                ('weight', 15, urwid.Text(('email_from', email['from'].fn))),
-                (6,            urwid.Text(('email_attrs', '(    )'))),
-                ('weight', 27, urwid.Text(('email_subject', email['subject']))),
-                (10,           urwid.Text(('email_date', '2021-01-01')))],
-                dividechars=1)
-            widgets.append(Selectable(cols, on_select=_sel_email(email)))
-
-        urwid.ListBox.__init__(self, urwid.SimpleListWalker(widgets))
+              ('weight', 15, urwid.Text(('email_from', frm), wrap='clip')),
+              (6,            urwid.Text(('email_attrs', attrs))),
+              ('weight', 27, urwid.Text(('email_subject', subj), wrap='clip')),
+              (10,           urwid.Text(('email_date', dt)))],
+              dividechars=1)
+            return Selectable(cols,
+                on_select=lambda x: self.tui_frame.show_email(self.emails[pos]))
+        except:
+            traceback.print_exc()
+        raise IndexError
 
 
 class ContextList(urwid.ListBox):
@@ -160,21 +203,25 @@ class TuiFrame(urwid.Frame):
         self.app_bridge = None
 
         self.left = ContextList(self, test_contexts)
-        self.right = urwid.Filler(urwid.Text(
-            [ENVELOPES], 'center'),
-            valign='middle')
         self.middle = urwid.Filler(urwid.Text(
             [HELLO, ('subtle', HELLO_CREDITS)], 'center'),
+            valign='middle')
+        self.right = urwid.Filler(urwid.Text(
+            [ENVELOPES], 'center'),
             valign='middle')
 
         self.default_topbar()
         self.visible_columns = 0
+        self.swap_visible = False
         self.update_columns(update=False)
 
         urwid.Frame.__init__(self, self.columns, header=self.topbar)
 
-    async def incoming_message(self, message):
-        print('Got message: %s' % message)
+    def incoming_message(self, message):
+        message = json.loads(message)
+        for _, _, _, widget in self.history:
+            if widget is not None:
+                widget.incoming_message(message)
 
     def link_bridge(self, app_bridge):
         self.app_bridge = app_bridge
@@ -185,17 +232,36 @@ class TuiFrame(urwid.Frame):
         if self.visible_columns > 1:
             self.left = widget
         else:
-            self.history.append((time.time(), 'middle', self.middle))
+            self.history.append((time.time(), 'middle', self.middle, None))
             self.middle = widget
         self.update_columns()
 
     def show_tag(self, which):
-        self.history.append((time.time(), 'middle', self.middle))
-        self.middle = EmailList(self, test_emails)
+        elw = EmailListWalker(self, None, test_emails)  # FIXME
+        self.history.append((time.time(), 'middle', self.middle, elw))
+        #self.app_bridge.send_json(RequestTag(which))
+        self.middle = urwid.ListBox(elw)
         self.update_columns()
 
-    def show_email(self, which):
-        pass
+    def show_mailbox(self, which):
+        elw = EmailListWalker(self, which)
+        self.history.append((time.time(), 'middle', self.middle, elw))
+        self.middle = urwid.ListBox(elw)
+        self.update_columns()
+
+    def show_search_result(self, which):
+        elw = EmailListWalker(self, None, test_emails)  # FIXME
+        self.history.append((time.time(), 'middle', self.middle, elw))
+        #self.app_bridge.send_json(RequestSearch(which))
+        self.middle = urwid.ListBox(elw)
+        self.update_columns()
+
+    def show_email(self, metadata):
+        edw = EmailDisplay(self, metadata, None)
+        self.history.append((time.time(), 'right', self.right, edw))
+        self.app_bridge.send_json(RequestEmail(metadata, text=True))
+        self.right = edw
+        self.update_columns()
 
     def max_child_rows(self):
         return self.screen.get_cols_rows()[1] - 2
@@ -232,11 +298,18 @@ class TuiFrame(urwid.Frame):
         if cols > sum(self.column_needs[:2])+1:
           columns.append(
             ('fixed', self.column_needs[0], _w(_b(self.left), 'sidebar')))
-        columns.append(
-            ('weight', self.column_needs[1], _w(_b(self.middle), 'content')))
         if cols >= sum(self.column_needs)+2:
           columns.append(
+            ('weight', self.column_needs[1], _w(_b(self.middle), 'content')))
+          columns.append(
             ('weight', self.column_needs[2], _w(_b(self.right), 'sidebar')))
+        else:
+          if self.swap_visible:
+            columns.append(
+              ('weight', self.column_needs[2], _w(_b(self.right), 'content')))
+          else:
+            columns.append(
+              ('weight', self.column_needs[1], _w(_b(self.middle), 'content')))
 
         self.visible_columns = len(columns)
         self.columns = urwid.Columns(columns, dividechars=1)
@@ -246,39 +319,54 @@ class TuiFrame(urwid.Frame):
     def unhandled_input(self, key):
         if key == 'q':
             raise urwid.ExitMainLoop()
+        if key == 'right':
+            self.swap_visible = not self.swap_visible
+        self.update_columns()
 
 
-def Main(workdir, args):
-    app_worker = AppWorker(workdir).connect()
-    aev_loop = asyncio.get_event_loop()
-    app_bridge = None
+def Main(workdir, sys_args, tui_args, send_args):
+    app_bridge = app_worker = None
     try:
-        tui_palette = [
-            (None,             'light gray',  'black',     ''),
-            ('',               'light gray',  'black',     ''),
-            ('body',           'light gray',  'black',     ''),
-            ('sidebar',        'light gray',  'black',     ''),
-            ('content',        'light gray',  'black',     ''),
-            ('email',          'brown',       'black',     ''),
-            ('active',         'light blue',  'black',     ''),
-            ('active',         'white',       'brown',     ''),
-            ('hotkey',         'brown',       'black',     ''),
-            ('act_hk',         'black',       'brown',     ''),
-            ('crumbs',         'white',       'dark blue', ''),
-            ('header',         'light gray',  'black',     ''),
-            ('top_hk',         'brown',       'black',     ''),
-            ('subtle',         'dark gray',   'black',     ''),
-            ('email_from',     'light gray',  'black',     ''),
-            ('email_attrs',    'dark gray',   'black',     ''),
-            ('email_subject',  'light gray',  'black',     ''),
-            ('email_date',     'dark gray',   'black',     ''),
-            ('focus',          'white',       'dark blue', '')]
-
+        app_worker = AppWorker(workdir).connect()
         screen = urwid.raw_display.Screen()
         tui_frame = TuiFrame(screen)
+        aev_loop = asyncio.get_event_loop()
         app_bridge = AsyncRPCBridge(aev_loop, app_worker, tui_frame)
 
-        urwid.MainLoop(urwid.AttrMap(tui_frame, 'body'), tui_palette,
+        # Request "locked" status from the app.
+        app_crypto_status = app_worker.call('rpc/crypto_status')
+        app_is_locked = app_crypto_status.get('locked')
+
+        print('APP IS%s LOCKED' % ('' if app_is_locked else ' NOT'))
+
+        if not app_is_locked:
+            jsr = JMAPSessionResource(app_worker.call('rpc/jmap_session'))
+            print(jsr)
+            # Request list of available JMAP Sessions from the app.
+            # Establish a websocket/JMAP connection to each Session.
+            # Populate sidebar.
+            pass  # FIXME
+
+        if send_args['_order']:
+            # Display the composer
+            # (Note, if locked, then "send" will just queue the messasge)
+            pass  # FIXME
+
+        elif '-f' in tui_args:
+            # Display the contents of a mailbox; this should always be
+            # possible whether app is locked or not.
+            tui_frame.show_mailbox(tui_args['-f'])
+
+        elif not app_is_locked:
+            # Display default Session/INBOX
+            tui_frame.show_tag('inbox')  # FIXME
+
+        else:
+            # Display locked screen
+            pass # FIXME
+
+        urwid.MainLoop(urwid.AttrMap(tui_frame, 'body'),
+            palette(app_worker.app.config),
             pop_ups=True,
             screen=screen,
             handle_mouse=False,
@@ -292,5 +380,5 @@ def Main(workdir, args):
         if app_bridge:
             # FIXME: This is probably not enough
             app_bridge.keep_running = False
-        if app_worker.is_alive():
+        if app_worker and app_worker.is_alive():
             app_worker.quit()
---
+++ moggie/app/tui/decorations.py
@@ -0,0 +1,66 @@
+from ...config import APPNAME, APPVER
+
+
+ENVELOPES = ("""\
+     _______      x
+    |==   []|     x
+    |  ==== |____ x
+    '-------'  []|x
+         |   === |x
+         '-------'x
+  _______         x
+ |==   []|        x
+ |  ==== |        x
+ '-------'        x
+""").replace('x', '')
+
+HELLO = ("""\
+  _                        x
+  \`*-.                    x
+   )  _`-.                 x
+  .  : `. .                x
+  : _   '  \               x
+  ; *` _.   `*-._          x
+  `-.-'          `-.       x
+    ;       `       `.     x
+    :.       .        \    x
+    . \  .   :   .-'   .   x
+    '  `+.;  ;  '      :   x
+    :  '  |    ;       ;-. x
+    ; '   : :`-:     _.`* ;x
+.*' /  .*' ; .*`- +'  `*'  x
+ `*-*   `*-*  `*-*'        x
+
+%s v%s                     x
+""").replace('x', '') % (APPNAME, APPVER)
+
+HELLO_CREDITS = """
+   (cat by Blazej Kozlowski)
+"""
+
+
+# FIXME: generate different palettes based on the contents of our
+#        config file; we should let the user specify their own
+#        colors, and also provide light/dark themes.
+def palette(config):
+    return [
+            (None,             'light gray',  'black',     ''),
+            ('',               'light gray',  'black',     ''),
+            ('body',           'light gray',  'black',     ''),
+            ('sidebar',        'light gray',  'black',     ''),
+            ('content',        'light gray',  'black',     ''),
+            ('email',          'brown',       'black',     ''),
+            ('active',         'light blue',  'black',     ''),
+            ('active',         'white',       'brown',     ''),
+            ('hotkey',         'brown',       'black',     ''),
+            ('act_hk',         'black',       'brown',     ''),
+            ('crumbs',         'white',       'dark blue', ''),
+            ('header',         'light gray',  'black',     ''),
+            ('top_hk',         'brown',       'black',     ''),
+            ('subtle',         'dark gray',   'black',     ''),
+            ('email_from',     'light gray',  'black',     ''),
+            ('email_attrs',    'dark gray',   'black',     ''),
+            ('email_subject',  'light gray',  'black',     ''),
+            ('email_date',     'dark gray',   'black',     ''),
+            ('focus',          'white',       'dark blue', '')]
+
